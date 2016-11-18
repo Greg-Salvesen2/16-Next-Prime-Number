73f396d1be740ff4772c2024d918e6deb50ab50e
Greg Salvesen <greg@latenightdesigns.com>
2016-11-18 10:44:35 -0500

    Initial Commit

---
+++ 16 Next Prime Number/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  16 Next Prime Number
+//
+//  Created by Greg Salvesen on 11/18/16.
+//  Copyright © 2016 DVS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
